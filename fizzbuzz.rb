78d26c04a4e15c68cf7b3557239dc685c1cdd687
Courtney Haltiwanger <chalt0319@gmail.com>
2018-05-30 17:26:19 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(number)
+  if number == 3 
+    puts "Fizz"
+  elsif number == 5
+    puts "Buzz"
+  elsif number == 15
+    puts "FizzBuzz"
+  elsif number == 4 
+    puts "nil"
+end
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+fizz_4 = fizzbuzz(4)
+fizz_3 = fizzbuzz(3)
