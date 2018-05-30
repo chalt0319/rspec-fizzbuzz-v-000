58a191f87a562f929e89489723beaf416b168f04
Courtney Haltiwanger <chalt0319@gmail.com>
2018-05-30 18:00:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,11 @@
 def fizzbuzz(number)
   if number % 3 == 0 
-    puts "Fizz"
+    return "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif (number % 5 == 0 && number % 3)
-    puts "FizzBuzz"
+    return "FizzBuzz"
   else 
     puts "nil"
   end
 end
-
-fizz_5 = fizzbuzz(5)
-fizz_15 = fizzbuzz(15)
-fizz_4 = fizzbuzz(4)
-fizz_3 = fizzbuzz(3)
